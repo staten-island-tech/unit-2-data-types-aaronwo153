2b5d0a0a4d136cc8941ce6420feae1d0f3828db4
aaronwo153 <aaronw153@nycstudents.net>
2024-09-17 10:13:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-"test"
-["t","e","s","t"]
+# x = input('What do you want to write? ')
+# y= x.split( )
+# print(len(y))
+
+x = input('I     into the house. ')
+y= x.split( )
+insert(1)
+print(input)
+
