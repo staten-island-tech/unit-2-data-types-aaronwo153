ba1b4a75b18fa365bad516e308579b1a482e467e
aaronwo153 <aaronw153@nycstudents.net>
2024-09-18 10:13:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,24 @@
 # y= x.split( )
 # print(len(y))
 
-x = input('I     into the house. ')
-y= x.split( )
-insert(1)
-print(input)
+# Mad Libs Project
+# x = input('Today I _____ into my house. Write a verb. ')
+# print(f"Today I {x} into my house.")
+# y = input('After that I saw  ______ eating lunch. Write a celebrity name. ')
+# print(f"Today I {x} into house. After that, I saw {y} eating lunch.")
+# u = input('They were eating ___ hamburger(s)! Write a number. ')
+# print(f"Today I {x} into house. After that, I saw {y} eating lunch. They were eating {u} hamburger(s)!")
+# q = input('Later when I went outside, I saw a ____ roll into a sinkhole! Write a noun. ')
+# print(f"Today I {x} into house. After that, I saw {y} eating lunch. They were eating {u} hamburger(s)! Later when I went outside, I saw a {q} roll into a sinkhole!")
+# t = input('I decided to ____ in and eat it! Write a verb. ')
+# print(f"Today I {x} into house. After that, I saw {y} eating lunch. They were eating {u} hamburger(s)! Later when I went outside, I saw a {q} roll into a sinkhole! I decided to {t} in and eat it!")
+# i = input('Down in the hole, I saw a ____ husky. Write an adjective. ')
+# print(f"Today I {x} into house. After that, I saw {y} eating lunch. They were eating {u} hamburger(s)! Later when I went outside, I saw a {q} roll into a sinkhole! I decided to {t} in and eat it! Down in the hole, I saw a {i} husky.")
+# o = input('That husky had ___ legs! Write a number. ')
+# print(f"Today I {x} into house. After that, I saw {y} eating lunch. They were eating {u} hamburger(s)! Later when I went outside, I saw a {q} roll into a sinkhole! I decided to {t} in and eat it! Down in the hole, I saw a {i} husky. That husky had {o} legs!")
 
+remainder = 0
+if remainder < 0:
+    print('odd')
+elif remainder == 0:
+    print('even')
